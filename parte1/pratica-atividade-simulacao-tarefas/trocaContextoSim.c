350db1bec3e28ad98a70c1b63410e123d6de0a0c
ga-bun <carvalhodegabriele@gmail.com>
2026-09-23 19:24:04 -0300

    atividade simulação tarefas inicio

---
+++ parte1/pratica-atividade-simulacao-tarefas/trocaContextoSim.c
@@ -0,0 +1,10 @@
+// Implementar, em linguagem C, uma simulação simplificada do gerenciamento de tarefas de um Sistema Operacional.
+// O programa dever a:
+    // 1 Criar 3 tarefas;
+    // 2 Representar cada tarefa por uma TCB;
+    // 3 Armazenar as tarefas em um vetor, representando uma fila;
+    // 4 Executar as tarefas de forma alternada;
+    // 5 Realizar a troca de contexto ap ́os um determinado quantum;
+    // 6 Preservar o contexto de cada tarefa para que ela continue exatamente de onde parou.
+// Importante
+// A troca de contexto sera simulada. Nao havera manipulacao real dos registradores do processado
